64155c4343549e25cee84cc53138b427958bbce8
Zhenya Shvyrkov <zhenya100694@mail.ru>
2020-08-03 23:01:30 +0300

    Re-adding base entity

--- src/main/java/com/ZhenyaShvyrkov/javacore/chapter05/task2/FizzBuzz.java
+++ src/main/java/com/ZhenyaShvyrkov/javacore/chapter05/task2/FizzBuzz.java
@@ -1,5 +1,8 @@
 package main.java.com.ZhenyaShvyrkov.javacore.chapter05.task2;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 class FizzBuzz{
     final int n;
 
@@ -21,5 +24,103 @@ class FizzBuzz{
 
     public void number(Runnable n) {
         n.run();
+    }
+    public static void main(String[] args) throws InterruptedException {
+        FizzBuzz fizzBuzz = new FizzBuzz(15);
+        int [] array = new int[1];
+        CyclicBarrier barrier = new CyclicBarrier(4);
+
+        Thread threadA = new Thread(() -> {
+            fizzBuzz.fizz(() -> {
+                for(int i = 1; i <= fizzBuzz.n; i++) {
+                    array[0] = i;
+                    if(array[0] % 3 == 0 && array[0] % 5 == 0) {}
+                    else if (array[0] % 3 == 0) {
+                        System.out.print("fizz");
+                        if(i != fizzBuzz.n ){
+                            System.out.print(", ");
+                        }
+                    }
+                    try {
+                        barrier.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        });
+        Thread threadB = new Thread(() -> {
+            fizzBuzz.fizz(() -> {
+                for(int i = 1; i <= fizzBuzz.n; i++) {
+                    array[0] = i;
+                    if(array[0] % 3 == 0 && array[0] % 5 == 0) {}
+                    else if (array[0] % 5 == 0) {
+                        System.out.print("buzz");
+                        if(i != fizzBuzz.n){
+                            System.out.print(", ");
+                        }
+                    }
+
+                    try {
+                        barrier.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        });
+        Thread threadC = new Thread(() -> {
+            fizzBuzz.fizz(() -> {
+                for(int i = 1; i <= fizzBuzz.n; i++) {
+                    array[0] = i;
+                    if (array[0] % 3 == 0 && array[0] % 5 == 0) {
+                        System.out.print("fizzbuzz");
+                        if(i != fizzBuzz.n){
+                            System.out.print(", ");
+                        }
+                    }
+                    try {
+                        barrier.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        });
+        Thread threadD = new Thread(() -> {
+            fizzBuzz.fizz(() -> {
+                for(int i = 1; i <= fizzBuzz.n; i++) {
+                    array[0] = i;
+                    if (array[0] % 3 != 0 && array[0] % 5 != 0) {
+                        System.out.print(array[0]);
+                        if(i != fizzBuzz.n){
+                            System.out.print(", ");
+                        }
+                    }
+                    try {
+                        barrier.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        });
+        threadA.start();
+        threadB.start();
+        threadC.start();
+        threadD.start();
+
+        threadA.join();
+        threadB.join();
+        threadC.join();
+        threadD.join();
     }
 }
